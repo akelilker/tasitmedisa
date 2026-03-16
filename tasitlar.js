a50cabc62899b7758ad2a4e8c4256c2dbe6386f6
ilker akel <ilkerakel@gmail.com>
2026-03-16 13:12:08 +0300

    Update tasitlar.js

--- tasitlar.js
+++ tasitlar.js
@@ -504,6 +504,9 @@
     }
 
     const isDataLoaded = !!window.__medisaDataLoaded;
+    // #region agent log
+    fetch('http://127.0.0.1:7824/ingest/04dd9237-7037-48c1-b605-adbae39c06ee',{method:'POST',headers:{'Content-Type':'application/json','X-Debug-Session-Id':'13e385'},body:JSON.stringify({sessionId:'13e385',location:'tasitlar.js:openVehiclesModal',message:'openVehiclesModal',data:{isDataLoaded,willShowLoading:!isDataLoaded},timestamp:Date.now(),hypothesisId:'H4'})}).catch(function(){});
+    // #endregion
     if (!isDataLoaded && typeof window.loadDataFromServer === 'function') {
       if (content) {
         content.innerHTML = '<div class="vehicles-loading-state">Taşıtlar yükleniyor...</div>';
