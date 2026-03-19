52cad1c5395667fa17527bbaacd4fb60c28e19d7
ilker akel <ilkerakel@gmail.com>
2026-03-19 15:08:45 +0300

    ö

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Medisa Taşıt Yönetim Sistemi
 // Version 2.11 - Activate: claim önce (InvalidStateError düzeltmesi); syntax temiz
 
-const CACHE_VERSION = 'medisa-v2.19';
+const CACHE_VERSION = 'medisa-v2.20';
 
 // Subpath desteği: /medisa/sw.js ise base = '/medisa', kök deploy'da base = ''
 function getBase() {
