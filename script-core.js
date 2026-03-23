c44c6cc2b0402fb9181cd67c3d359d49f40c95e3
ilker akel <ilkerakel@gmail.com>
2026-03-23 17:53:52 +0300

    hh

--- script-core.js
+++ script-core.js
@@ -507,13 +507,13 @@ document.addEventListener('DOMContentLoaded', () => {
 // tasitlar.js / raporlar.js / kayit.js / ayarlar.js yüklendiğinde kendi open* implementasyonlarını yazar.
 (function() {
   var TASITLAR_JS = 'tasitlar.js?v=20260321.2';
-  var TASITLAR_CSS = 'tasitlar.css?v=20260323.2';
+  var TASITLAR_CSS = 'tasitlar.css?v=20260323.4';
   var RAPORLAR_JS = 'raporlar.js?v=20260320.4';
-  var RAPORLAR_CSS = 'raporlar.css?v=20260323.3';
+  var RAPORLAR_CSS = 'raporlar.css?v=20260323.4';
   var KAYIT_JS = 'kayit.js?v=20260323.1';
-  var KAYIT_CSS = 'kayit.css?v=20260323.2';
+  var KAYIT_CSS = 'kayit.css?v=20260323.4';
   var AYARLAR_JS = 'ayarlar.js?v=20260319';
-  var AYARLAR_CSS = 'ayarlar.css?v=20260322.1';
+  var AYARLAR_CSS = 'ayarlar.css?v=20260323.4';
 
   window.openVehiclesView = function() {
     showModuleSpinner();
