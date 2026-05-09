b78f6912648e926de16aeb8ff1e01d2cef10c70c
akelilker <ilkerakel@gmail.com>
2026-05-10 02:59:35 +0300

    2

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Service Worker - Medisa Taşıt Yönetim Sistemi
 // Version 2.15 - Güvenli offline okuma kabuğu
 
-const CACHE_VERSION = 'medisa-v2.83';
+const CACHE_VERSION = 'medisa-v2.84';
 // Raporlar modülü (raporlar.js / raporlar.css): ana shell cache'inden ayrı; MEDISA_MODULE_VERSIONS.raporlar ile senkron tut
-const CACHE_RAPORLAR_VERSION = 'medisa-raporlar-20260508.5';
+const CACHE_RAPORLAR_VERSION = 'medisa-raporlar-20260508.6';
 
 function isRaporlarModuleAssetUrl(u) {
   return /\/raporlar\.(js|css)$/i.test(u.pathname);
