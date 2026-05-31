b6b479c1198a3956d98d2bcd7ae34c32bdad1e47
akelilker <ilkerakel@gmail.com>
2026-05-31 13:22:47 +0300

    fd

--- script-core.js
+++ script-core.js
@@ -971,7 +971,7 @@ var MEDISA_MODULE_VERSIONS = {
   kayitJs: '20260528.1',
   kayitCss: '20260531.1',
   ayarlarJs: '20260531.1',
-  ayarlarCss: '20260531.4',
+  ayarlarCss: '20260531.5',
   tasitlarYazici: '20260517.5'
 };
 window.MEDISA_MODULE_VERSIONS = MEDISA_MODULE_VERSIONS;
