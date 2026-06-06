c639ac062932c41cf7348e6761b9724da9aef79d
akelilker <ilkerakel@gmail.com>
2026-06-06 16:18:56 +0300

    renk

--- tasitlar.js
+++ tasitlar.js
@@ -4974,7 +4974,7 @@
       el.innerHTML =
         '<div class="modal-container monthly-todo-modal-container">' +
           '<div class="modal-header">' +
-            '<h2>' + escapeHtml(getMonthlyTodoTitleText()) + '</h2>' +
+            '<h2 class="premium-title">' + escapeHtml(getMonthlyTodoTitleText()) + '</h2>' +
             '<button type="button" class="modal-close monthly-todo-modal-close" aria-label="Kapat">' +
               '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>' +
             '</button>' +
