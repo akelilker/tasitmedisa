9fccf5d3d0f3911c53f038209e95f86c367c73ad
akelilker <ilkerakel@gmail.com>
2026-07-18 22:34:28 +0300

    Update tasitlar.js

--- tasitlar.js
+++ tasitlar.js
@@ -164,7 +164,7 @@
 
 
 (function() {
-  const MEDISA_TASITLAR_MODULE_VERSION = '20260708.7';
+  const MEDISA_TASITLAR_MODULE_VERSION = '20260718.3';
   window.__medisaTasitlarModuleReady = false;
   window.__medisaTasitlarModuleVersion = MEDISA_TASITLAR_MODULE_VERSION;
 
